8be9d72708c73c26eb0d46db63690d2fac0c6a84
prabhjyot009 <prabh156dps@gmail.com>
2024-03-06 17:55:39 +0530

    Refactored Practice.cpp and fixed boundary fill algorithm

--- Practice.cpp
+++ Practice.cpp
@@ -1,22 +1,40 @@
-#include <graphics.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <math.h>
-void floodfill8(int x, int y, int fill, int old) {
-    int current = getpixel(x, y);
-    if (current == old) {
-        putpixel(x, y, fill);        
-        floodfill8(x + 1, y, fill, old);
-        floodfill8(x - 1, y, fill, old);
-        floodfill8(x, y + 1, fill, old);
-        floodfill8(x, y - 1, fill, old);
+#include<stdio.h>
+#include<graphics.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+void boundaryfill4(int x,int y,int fill,int boundary){
+    int current=getpixel(x,y);
+    if(current!=boundary && current!=fill){
+        putpixel(x,y,fill);
+        boundaryfill4(x,y+1,fill,boundary);
+        boundaryfill4(x+1,y,fill,boundary);
+        boundaryfill4(x-1,y,fill,boundary);
+        boundaryfill4(x,y-1,fill,boundary);
     }
 }
 int main(){
-int gd=DETECT,gm;
-initgraph(&gd,&gm,(char*)"");
-circle(66,66,10);
-floodfill8(66,66,15,0);
-getch();
+    int gd=DETECT,gm;
+    initgraph(&gd,&gm,(char*)"");
+    int x1,y1,x2,y2,x,y,fill,boundary;
+    printf("Enter value of x1:");
+    scanf("%d",&x1);
+    printf("Enter value of y1:");
+    scanf("%d",&y1);
+    printf("Enter value of x2:");
+    scanf("%d",&x2);
+    printf("Enter value of y2:");
+    scanf("%d",&y2);
+    printf("Enter value of x:");
+    scanf("%d",&x);
+    printf("Enter value of y:");
+    scanf("%d",&y);
+    printf("Fill:");
+    scanf("%d",&fill);
+    printf("Boundary:");
+    scanf("%d",&boundary);
+    rectangle(x1,y1,x2,y2);
+    boundaryfill4(x,y,fill,boundary);
+    getch();
+    delay(10000);
 }
